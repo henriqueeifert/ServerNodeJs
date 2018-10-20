9e15f57143aa7a3d89977bbde74284f854c94323
henriqueeifert <henriquemteifert@gmail.com>
2018-10-19 23:29:38 -0300

    Teste 02

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json({
 app.use(bodyParser.urlencoded({
     extended: false }));
 //
-app.use(function(req,res,next){ 
+app.use(function(req,res,next){
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Headers','Content-Type','x-access-token','Accept','Origin','X-Requested-with');
     res.header('Access-Control-Allow-Methods','GET','POST', 'DELETE','PUT');
