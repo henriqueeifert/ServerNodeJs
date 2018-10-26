4e0ce73e5824ab0f950d481c2d14a56f1d20c92a
henriqueeifert <henriquemteifert@gmail.com>
2018-10-25 21:27:46 -0300

    message

--- src/validators/fluent-validator.js
+++ src/validators/fluent-validator.js
@@ -6,30 +6,30 @@ function ValidationContract() {
     errors = [];
 }
 
-ValidationContract.prototype.isRequired = (campo, value, message) => {
+ValidationContract.prototype.isRequired = (value, message) => {
     if (!value || value.length <= 0)
-        errors.push({ campo: message });
+        errors.push({ message: message });
 }
 
-ValidationContract.prototype.hasMinLen = (campo, value, min, message) => {
+ValidationContract.prototype.hasMinLen = (value, min, message) => {
     if (!value || value.length < min)
-        errors.push({ campo: message });
+        errors.push({ message: message });
 }
 
-ValidationContract.prototype.hasMaxLen = (campo, value, max, message) => {
+ValidationContract.prototype.hasMaxLen = (value, max, message) => {
     if (!value || value.length > max)
-        errors.push({ campo: message });
+        errors.push({ message: message });
 }
 
-ValidationContract.prototype.isFixedLen = (campo, value, len, message) => {
+ValidationContract.prototype.isFixedLen = (value, len, message) => {
     if (value.length != len)
-        errors.push({ campo: message });
+        errors.push({ message: message });
 }
 
-ValidationContract.prototype.isEmail = (campo, value, message) => {
+ValidationContract.prototype.isEmail = (value, message) => {
     var reg = new RegExp(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/);
     if (!reg.test(value))
-        errors.push({ campo: message });
+        errors.push({ message: message });
 }
 
 ValidationContract.prototype.errors = () => { 
