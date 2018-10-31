76ec7787e578de06f26550646a3012e0e679aa36
henriqueeifert <henriquemteifert@gmail.com>
2018-10-31 18:19:11 -0300

    remove id

--- src/models/user.js
+++ src/models/user.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
+    /*
     id: {
         type: Number,
         required: true
     },
+    */
     nome: {
         type: String,
         required: true
