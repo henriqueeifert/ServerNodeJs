9386e18550c8a212155a3d49748c7b28cec1a575
henriqueeifert <henriquemteifert@gmail.com>
2018-10-31 22:57:59 -0300

    correção GetByID

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -6,7 +6,7 @@ const controller  = require('../controllers/user-controller');
 const authService = require('../services/auth-service');
 
 router.get('/',controller.get);
-router.get('/:id',controller.getId);
+router.get('/:id',controller.getById);
 router.post('/',controller.post);
 //router.post('/authenticate', controller.authenticate);
 //router.post('/refreshToken', authService.authorize, controller.refreshToken);
