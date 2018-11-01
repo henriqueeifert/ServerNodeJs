4ad54f4c996c1a3289e6dbcb06277aa18322f2df
henriqueeifert <henriquemteifert@gmail.com>
2018-11-01 19:22:14 -0300

    deploy

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -216,6 +216,7 @@ exports.get = async(req, res, next) => {
         res.status(200).send(
             {
                 usuarios: user
+                        
         });
     } catch (e) {
         res.status(500).send({
