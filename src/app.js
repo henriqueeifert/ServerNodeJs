3d0c72d7133308ba888066ab39bcbb659e2ffabc
henriqueeifert <henriquemteifert@gmail.com>
2018-11-18 22:58:31 -0200

    Access-Control-Allow-Methods

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(function(req,res,next){
     //res.type('application/json');
     res.header();
     //res.header('Access-Control-Allow-Headers','Content-Type'/*'*','x-access-token','X-Requested-With','Accept','Origin','X-Requested-with'*/);
-    //res.header('Access-Control-Allow-Methods','GET','POST', 'DELETE','PUT');
+    res.header('Access-Control-Allow-Methods','GET','POST', 'DELETE','PUT');
     next();
 })
 
