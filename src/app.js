72d8248f6b2805d804b603d4739320aa2392400c
henriqueeifert <henriquemteifert@gmail.com>
2018-11-18 23:17:56 -0200

    allow-headers

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use(function(req,res,next){
     res.header('Access-Control-Allow-Origin','*');
     //res.type('application/json');
     //res.header();     
-    res.header('Access-Control-Allow-Headers','Content-Type'/*'*','x-access-token','X-Requested-With','Accept','Origin','X-Requested-with'*/);
+    res.header('Access-Control-Allow-Headers','*'/*'Content-Type','x-access-token','X-Requested-With','Accept','Origin','X-Requested-with'*/);
     res.header('Access-Control-Allow-Methods','GET','POST', 'DELETE','PUT');
     next();
 })
