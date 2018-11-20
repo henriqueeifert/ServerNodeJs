99d88b113633598ca18bc6385c8415414cfdc554
henriqueeifert <henriquemteifert@gmail.com>
2018-11-20 19:07:57 -0200

    teste fábio

--- src/app.js
+++ src/app.js
@@ -28,12 +28,29 @@ app.use(bodyParser.urlencoded({
     extended: false }));
 //
 app.use(function(req,res,next){
-    res.header('Access-Control-Allow-Origin','*');    
-    //res.header();     
-    res.header('Access-Control-Allow-Headers','*'/*'Content-Type','x-access-token','X-Requested-With','Accept','Origin','X-Requested-with'*/);
-    res.header('Access-Control-Allow-Methods','GET','POST', 'DELETE','PUT');
-    res.type('application/json');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'null');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
     next();
+    
+    //req.header('Access-Control-Allow-Origin');    
+    //res.header();     
+    //res.header('Access-Control-Allow-Headers','Content-Type'/*,'x-access-token','X-Requested-With','Accept','Origin','X-Requested-with'*/);
+    //req.header('Access-Control-Allow-Methods','GET','POST', 'DELETE','PUT');
+    //res.type('application/json');
+    //next();
+    
 })
 
 app.use('/', indexRoute);
