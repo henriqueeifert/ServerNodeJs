612caf2847cb2dee38e6dd5de018b22f438a12b2
henriqueeifert <henriquemteifert@gmail.com>
2018-11-21 20:13:42 -0200

    deploy parcial

--- src/controllers/calcular-controller.js
+++ src/controllers/calcular-controller.js
@@ -5,8 +5,23 @@ const mongoose = require('mongoose');
 
 exports.post = async(req, res, next) => {
     try {   
-        /* */
-        next();
+        var qtPergutas = 0;
+        for (var i = 0; i < req.body.perguntas.length; i++) {
+            var idPergunta = req.body.perguntas[i];
+            if (idPergunta) {
+                qtPergutas = qtPergutas + 1;
+            }
+            console.log(idPergunta);
+        }    
+        //
+        console.log(qtPergutas); 
+        res.status(200).send({
+            mensagem: "Resultado Calculado com Sucesso",
+                resultado: {
+                    pontuacao: +qtPergutas
+                }
+        });
+
     }catch (e) {
         res.status(500).send({
             mensagem: 'Falha ao processar sua requisição: '+e
