d835c5aaa10861451a0eb019669566ae9c489cba
henriqueeifert <henriquemteifert@gmail.com>
2018-11-22 00:11:05 -0200

    Origin

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use(bodyParser.urlencoded({
 //
 app.use(function(req,res,next){
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'null');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS, DELETE');
     // Request headers you wish to allow
